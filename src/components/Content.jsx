d4788bc3528d1ebbf0f89217dfc50b36c5aff38e
shreegna <shreegnakomire21@gmail.com>
2026-03-10 15:36:46 +0530

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,25 @@
+import { useState,useEffect } from "react"
+import axios from 'axios';
 function Content(){
-    return <h3>Products Page</h3>
+    const [count,setCount ]=useState(0)
+    const [Products,setProducts]=useState()
+    const increment=()=>{
+        setCount(count+1)
+
+    };
+
+    const decrement=()=>{
+        setCount(count-1)
+    };
+    const fetchProducts=()=>{
+
+    }
+useEffect(fetchProducts,[])
+
+    return (<div>
+        <h3>Products Page</h3>
+        <button onClick={decrement}>-</button> {count} <button onClick={increment}>+</button>
+    </div>
+    );
 }
 export default Content
