7e6ea3010578bb75bf9e029734f9bcffeb300dc3
shreegna <shreegnakomire21@gmail.com>
2026-03-10 20:40:34 +0530

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,25 +1,35 @@
-import { useState,useEffect } from "react"
-import axios from 'axios';
-function Content(){
-    const [count,setCount ]=useState(0)
-    const [Products,setProducts]=useState()
-    const increment=()=>{
-        setCount(count+1)
-
-    };
-
-    const decrement=()=>{
-        setCount(count-1)
-    };
-    const fetchProducts=()=>{
-
-    }
-useEffect(fetchProducts,[])
-
-    return (<div>
-        <h3>Products Page</h3>
-        <button onClick={decrement}>-</button> {count} <button onClick={increment}>+</button>
+import { useState, useEffect } from "react";
+import axios from "axios";
+function Content() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const fetchProducts = async () => {
+    const url = "http://localhost:5000/products";
+    const res = await axios.get(url);
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h3>Products Page</h3>
+      <button onClick={decrement}>-</button>
+      {count}
+      <button onClick={increment}>+</button>
+      <hr />
+      <ol>
+        {products.map((product) => (
+          <li>{product.name}</li>
+        ))}
+      </ol>
     </div>
-    );
+  );
 }
-export default Content
+export default Content;
