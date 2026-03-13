ef216e6d3eabc711e4cde930f6ca82a7dd54e1e2
shreegna <shreegnakomire21@gmail.com>
2026-03-13 19:41:38 +0530

    uppdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from "react";
+
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
@@ -7,28 +8,56 @@ import Logout from "./components/Logout";
 import Register from "./components/Register";
 import Cart from "./components/Cart";
 import Orders from "./components/Orders";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export const AppContext = createContext();
+
 function App() {
-  const [user, setUser] = useState({});
-  const [cart,setCart]=useState([]);
-  return (
-    <div>
-      <AppContext.Provider value={{ user, setUser,cart,setCart }}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route index element={<Content />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="login" element={<Login />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="register" element={<Register />} />
-            <Route path="orders" element={<Orders />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </AppContext.Provider>
-    </div>
-  );
+
+const [user, setUser] = useState({});
+const [cart, setCart] = useState([]);
+
+return (
+
+<div className="app-container">
+
+<AppContext.Provider value={{ user, setUser, cart, setCart }}>
+
+<BrowserRouter>
+
+<Header />
+
+<div className="main-content">
+
+<Routes>
+
+<Route index element={<Content />} />
+
+<Route path="cart" element={<Cart />} />
+
+<Route path="login" element={<Login />} />
+
+<Route path="logout" element={<Logout />} />
+
+<Route path="register" element={<Register />} />
+
+<Route path="orders" element={<Orders />} />
+
+</Routes>
+
+</div>
+
+<Footer />
+
+</BrowserRouter>
+
+</AppContext.Provider>
+
+</div>
+
+);
+
 }
+
 export default App;
